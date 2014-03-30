43a5c30743efcd38a1eec845db84073c2fb5e8d3
Paul Wenig <pwenig@gmail.com>
2014-03-30 11:13:47 -0600

    user sees welcome and add task link

--- spec/taskman_spec.rb
+++ spec/taskman_spec.rb
@@ -2,3 +2,11 @@ require "spec_helper"
 require "capybara/rspec"
 require_relative "../app"
 Capybara.app = App
+
+feature 'managing task list' do
+  scenario 'user can add tasks to a list' do
+    visit('/')
+    expect(page).to have_content "Welcome"
+    click_link "Add a task"
+  end
+end
